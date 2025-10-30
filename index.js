2abedf0d657d3c177bd687ffc6d11f32ef8a4552
Soumyaranjan Mahalik <124546914+srm1993@users.noreply.github.com>
2025-10-30 20:28:27 +0530

    Update CORS origin to production URL

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ mongoose.connect('mongodb+srv://soumyaranjan65432:kathakhunta90@cluster0.grybuzc
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://youtube-clone-frontend-plum.vercel.app',
     methods:['GET','POST','PUT','DELETE']
 }))
 app.use('/api',router);
